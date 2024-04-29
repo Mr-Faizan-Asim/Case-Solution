e891f05bf89f301170dc4a6b3c433a1dbe16f2a7
PC <mr.faizan.asim@gmail.comgit config --global user.email mr.faizan.asim@gmail.com>
2024-04-29 07:07:18 +0500

    first commit

---
+++ app/components/Dedicated/index.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+const Dedicated = () => {
+    return (
+        <div className="relative">
+
+            <Image src="/images/dedicated/spiral.svg" height={272} width={686} alt="spiral-design" className="absolute left-0 hidden lg:block -z-10" />
+
+            <div className='mx-auto max-w-7xl px-4 my-40 sm:py-20 lg:px-8'>
+                <div className='grid grid-cols-1 md:grid-cols-2 my-16'>
+
+                    {/* COLUMN-1 */}
+                    <div>
+                        <Image src="/images/dedicated/a.jpg" alt="man-icon" width={416} height={530} className="mx-auto md:mx-0" />
+                    </div>
+
+                    {/* COLUMN-2 */}
+                    <div className="relative">
+                        <Image src="images/dedicated/comma.svg" alt="comma-image" width={200} height={106} className="absolute comma-pos hidden lg:block" />
+                        <p className="text-2xl font-semibold mt-12 lg:ml-32 preline text-center lg:text-start"> Founder, Woix</p>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Dedicated;
